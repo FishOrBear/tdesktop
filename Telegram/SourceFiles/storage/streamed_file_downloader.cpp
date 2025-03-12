d2d8ea00c7caa4360b0818e0851e980c0d9d2435
ChenX <maple6@vip.qq.com>
2025-03-12 14:00:54 +0800

    添加请求次数

--- Telegram/SourceFiles/storage/streamed_file_downloader.cpp
+++ Telegram/SourceFiles/storage/streamed_file_downloader.cpp
@@ -16,7 +16,7 @@ namespace {
 using namespace Media::Streaming;
 
 constexpr auto kPartSize = Loader::kPartSize;
-constexpr auto kRequestPartsCount = 64;
+constexpr auto kRequestPartsCount = 64 * 4;
 
 } // namespace
 
